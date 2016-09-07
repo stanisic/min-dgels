fce42131302cadd4d85ac57d252807f9860e150c
Luka Stanisic <luka.stanisic@inria.fr>
2016-09-07 11:47:08 +0200

    adding all source code

---
+++ src/additional/multiple_regression.h
@@ -0,0 +1,6 @@
+#ifndef MULTIPLE_REGRESSION
+#define MULTIPLE_REGRESSION
+
+int dgels_(char *trans, integer *m, integer *n, integer *nrhs, doublereal *a, integer *lda, doublereal *b, integer *ldb, doublereal *work, integer *lwork, integer *info);
+
+#endif
